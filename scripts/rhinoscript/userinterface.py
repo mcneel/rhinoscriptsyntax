162bdec280e53b320f24c9025e2cba85fd7691f1
sbaer <steve@mcneel.com>
2011-01-24 09:26:59 -0800

    comments on option name restrictions in GetBoolean

--- scripts/rhinoscript/userinterface.py
+++ scripts/rhinoscript/userinterface.py
@@ -81,7 +81,8 @@ def GetBoolean(message, items, defaults):
     Parameters:
       message = a prompt
       items = list or tuple of options. Each option is a tuple of three strings
-        element 1 = description of the boolean value
+        element 1 = description of the boolean value. Must only consist of letters
+          and numbers. (no characters like space, period, or dash
         element 2 = string identifying the false value
         element 3 = string identifying the true value
       defaults = list of boolean values used as default or starting values
