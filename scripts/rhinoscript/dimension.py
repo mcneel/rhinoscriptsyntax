16c40619e2c9b87d23287dce3725f63076a3bc9f
sbaer <steve@mcneel.com>
2011-09-30 16:43:47 -0700

    Updated AddLeader to allow for a plane to be passed instead of a view

--- scripts/rhinoscript/dimension.py
+++ scripts/rhinoscript/dimension.py
@@ -52,14 +52,14 @@ def AddDimStyle(dimstyle_name=None):
     return ds.Name
 
 
-def AddLeader(points, view=None, text=None):
+def AddLeader(points, view_or_plane=None, text=None):
     """Adds a leader to the document. Leader objects are planar.
     The 3D points passed to this function should be co-planar
     Paramters:
       points = list of (at least 2) 3D points
-      view[opt] = If a view is specified, points will be constrained to the
-        view's construction plane. If a view is not specified, points will be
-        constrained to a plane fit through the list of points
+      view_or_plane[opt] = If a view is specified, points will be constrained
+        to the view's construction plane. If a view is not specified, points
+        will be constrained to a plane fit through the list of points
       text[opt] = leader's text string
     Returns:
       identifier of the new leader on success
@@ -68,15 +68,17 @@ def AddLeader(points, view=None, text=None):
     points = rhutil.coerce3dpointlist(points)
     if points is None or len(points)<2: raise ValueError("points must have at least two items")
     rc = System.Guid.Empty
-    if not view:
+    if not view_or_plane:
         if text is None:
             rc = scriptcontext.doc.Objects.AddLeader(points)
         else:
             if not isinstance(text, str): text = str(text)
             rc = scriptcontext.doc.Objects.AddLeader(text, points)
     else:
-        view = __viewhelper(view)
-        plane = view.ActiveViewport.ConstructionPlane()
+        plane = rhutil.coerceplane(view_or_plane)
+        if not plane:
+            view = __viewhelper(view_or_plane)
+            plane = view.ActiveViewport.ConstructionPlane()
         points2d = []
         for point in points:
             cprc, s, t = plane.ClosestParameter( point )
