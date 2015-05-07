f2892edc21b83ccdeff26c54479221a335c57d14
Alain <alain@mcneel.com>
2015-05-07 13:58:06 +0200

    fixes RH-30298 by testing for sc.id

--- Scripts/rhinoscript/surface.py
+++ Scripts/rhinoscript/surface.py
@@ -1994,20 +1994,23 @@ def TrimBrep(object_id, cutter, tolerance=None):
     else: cutter = rhutil.coerceplane(cutter, True)
     if tolerance is None: tolerance = scriptcontext.doc.ModelAbsoluteTolerance
     breps = brep.Trim(cutter, tolerance)
-    rhobj = rhutil.coercerhinoobject(object_id)
-    if rhobj:
-        attr = rhobj.Attributes
-        rc = []
-        for i in range(len(breps)):
-            if i==0:
-                scriptcontext.doc.Objects.Replace(rhobj.Id, breps[i])
-                rc.append(rhobj.Id)
-            else:
-                rc.append(scriptcontext.doc.Objects.AddBrep(breps[i], attr))
+    if scriptcontext.id == 2: #gh
+        return breps
     else:
-        rc = [scriptcontext.doc.Objects.AddBrep(brep) for brep in breps]
-    scriptcontext.doc.Views.Redraw()
-    return rc
+        rhobj = rhutil.coercerhinoobject(object_id)
+        if rhobj:
+            attr = rhobj.Attributes
+            rc = []
+            for i in range(len(breps)):
+                if i==0:
+                    scriptcontext.doc.Objects.Replace(rhobj.Id, breps[i])
+                    rc.append(rhobj.Id)
+                else:
+                    rc.append(scriptcontext.doc.Objects.AddBrep(breps[i], attr))
+        else:
+            rc = [scriptcontext.doc.Objects.AddBrep(brep) for brep in breps]
+        scriptcontext.doc.Views.Redraw()
+        return rc
 
 
 def TrimSurface( surface_id, direction, interval, delete_input=False):
