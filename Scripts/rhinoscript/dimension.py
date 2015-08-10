bd48a0446640736cbd22afea9e4ba18138857c20
Alain <alain@mcneel.com>
2015-08-10 10:21:05 +0200

    add arguments to AddLinearDimension docstring #RH-31058 Needs Testing

--- Scripts/rhinoscript/dimension.py
+++ Scripts/rhinoscript/dimension.py
@@ -100,6 +100,11 @@ def AddLeader(points, view_or_plane=None, text=None):
 
 def AddLinearDimension(plane, start_point, end_point, point_on_dimension_line):
     """Adds a linear dimension to the document
+    Parameters:
+      plane = The plane on which the dimension will lie.
+      start_point = The origin, or first point of the dimension.
+      end_point = The offset, or second point of the dimension.
+      point_on_dimension_line = A point that lies on the dimension line.
     Returns:
       identifier of the new object on success
       None on error
