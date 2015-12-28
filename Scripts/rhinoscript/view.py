fd14b9956c352ac7754067c49f8bb31bdb371ad9
alain <alain@mcneel.com>
2015-12-28 14:09:03 -0400

    fix 'mode' argument description in ViewProjection docstring #RH-32044

--- Scripts/rhinoscript/view.py
+++ Scripts/rhinoscript/view.py
@@ -1235,7 +1235,7 @@ def ViewProjection(view=None, mode=None):
     """Return or set a view's projection mode.
     Parameters:
       view:[opt] title or id of the view. If omitted, current active view is used
-      mode:[opt] the projection mode (1=parallel, 2=perspective)
+      mode:[opt] the projection mode (1=parallel, 2=perspective, 3=two point perspective)
     Returns:
       if mode is not specified, the current projection mode for the specified view
       if mode is specified, the previous projection mode for the specified view
