9f970a455e3ed7c071fe48e680c5f24fdc6e5082
Scott Davidson <scottd@mcneel.com>
2017-03-06 13:45:38 -0800

    Added Docstring information to Hatch.py

--- Scripts/rhinoscript/hatch.py
+++ Scripts/rhinoscript/hatch.py
@@ -6,14 +6,14 @@ import System.Guid
 def AddHatch(curve_id, hatch_pattern=None, scale=1.0, rotation=0.0):
     """Creates a new hatch object from a closed planar curve object
     Parameters:
-      curve_id = identifier of the closed planar curve that defines the
+      curve_id (guid): identifier of the closed planar curve that defines the
           boundary of the hatch object
-      hatch_pattern[opt] = name of the hatch pattern to be used by the hatch
+      hatch_pattern (str, optional): name of the hatch pattern to be used by the hatch
           object. If omitted, the current hatch pattern will be used
-      scale[opt] = hatch pattern scale factor
-      rotation[opt] = hatch pattern rotation angle in degrees.
+      scale (number, optional): hatch pattern scale factor
+      rotation (number, optional): hatch pattern rotation angle in degrees.
     Returns:
-      identifier of the newly created hatch on success
+      guid:identifier of the newly created hatch on success
       None on error
     Example:
       import rhinoscriptsyntax as rs
@@ -35,15 +35,15 @@ def AddHatch(curve_id, hatch_pattern=None, scale=1.0, rotation=0.0):
 def AddHatches(curve_ids, hatch_pattern=None, scale=1.0, rotation=0.0):
     """Creates one or more new hatch objects a list of closed planar curves
     Parameters:
-      curve_ids = identifiers of the closed planar curves that defines the
+      curve_ids ([guid, ...]): identifiers of the closed planar curves that defines the
           boundary of the hatch objects
-      hatch_pattern[opt] = name of the hatch pattern to be used by the hatch
+      hatch_pattern (str, optional):  name of the hatch pattern to be used by the hatch
           object. If omitted, the current hatch pattern will be used
-      scale[opt] = hatch pattern scale factor
-      rotation[opt] = hatch pattern rotation angle in degrees.
+      scale (number, optional): hatch pattern scale factor
+      rotation (number, optional): hatch pattern rotation angle in degrees.
     Returns:
-      identifiers of the newly created hatch on success
-      None on error
+      list(guid, ...): identifiers of the newly created hatch on success
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       curves = rs.GetObjects("Select closed planar curves", rs.filter.curve)
@@ -84,13 +84,13 @@ def AddHatchPatterns(filename, replace=False):
     """Adds hatch patterns to the document by importing hatch pattern definitions
     from a pattern file.
     Parameters:
-      filename = name of the hatch pattern file
-      replace[opt] = If hatch pattern names already in the document match hatch
+      filename (str): name of the hatch pattern file
+      replace (bool, optional): If hatch pattern names already in the document match hatch
           pattern names in the pattern definition file, then the existing hatch
           patterns will be redefined
     Returns:
-      Names of the newly added hatch patterns if successful
-      None on error
+      list(str, ...): Names of the newly added hatch patterns if successful
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       filename = rs.OpenFileName("Import", "Pattern Files (*.pat)|*.pat||")
@@ -117,11 +117,11 @@ def AddHatchPatterns(filename, replace=False):
 def CurrentHatchPattern(hatch_pattern=None):
     """Returns or sets the current hatch pattern file
     Parameters:
-      hatch_pattern[opt] = name of an existing hatch pattern to make current
+      hatch_pattern(str, optional):  name of an existing hatch pattern to make current
     Returns:
-      if hatch_pattern is not specified, the current hatch pattern
-      if hatch_pattern is specified, the previous hatch pattern
-      None on error
+      str: if hatch_pattern is not specified, the current hatch pattern
+      str: if hatch_pattern is specified, the previous hatch pattern
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       if rs.IsHatchPattern("Hatch2"): rs.CurrentHatchPattern("Hatch2")
@@ -143,11 +143,11 @@ def ExplodeHatch(hatch_id, delete=False):
     then planar face Brep objects will be created. Otherwise, line curve objects
     will be created
     Parameters:
-      hatch_id = identifier of a hatch object
-      delete[opt] = delete the hatch object
+      hatch_id (guid): identifier of a hatch object
+      delete (bool, optional): delete the hatch object
     Returns:
-      list of identifiers for the newly created objects
-      None on error
+      list(guid, ...): list of identifiers for the newly created objects
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select object")
@@ -177,13 +177,13 @@ def ExplodeHatch(hatch_id, delete=False):
 def HatchPattern(hatch_id, hatch_pattern=None):
     """Returns or changes a hatch object's hatch pattern
     Parameters:
-      hatch_id = identifier of a hatch object
-      hatch_pattern[opt] = name of an existing hatch pattern to replace the
+      hatch_id (guid): identifier of a hatch object
+      hatch_pattern (str, optional): name of an existing hatch pattern to replace the
           current hatch pattern
     Returns:
-      if hatch_pattern is not specified, the current hatch pattern
-      if hatch_pattern is specified, the previous hatch pattern
-      None on error
+      str: if hatch_pattern is not specified, the current hatch pattern
+      str: if hatch_pattern is specified, the previous hatch pattern
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       objects = rs.AllObjects()
@@ -213,10 +213,8 @@ def HatchPattern(hatch_id, hatch_pattern=None):
 
 def HatchPatternCount():
     """Returns the number of hatch patterns in the document
-    Parameters:
-      None
     Returns:
-      the number of hatch patterns in the document
+      number: the number of hatch patterns in the document
     Example:
       import rhinoscriptsyntax as rs
       print "There are", rs.HatchPatternCount(), "hatch patterns."
@@ -230,9 +228,9 @@ def HatchPatternDescription(hatch_pattern):
     """Returns the description of a hatch pattern. Note, not all hatch patterns
     have descriptions
     Parameters:
-      hatch_pattern = name of an existing hatch pattern
+      hatch_pattern (str): name of an existing hatch pattern
     Returns:
-      description of the hatch pattern on success otherwise None
+      str: description of the hatch pattern on success otherwise None
     Example:
       import rhinoscriptsyntax as rs
       patterns = rs.HatchPatternNames()
@@ -250,13 +248,14 @@ def HatchPatternDescription(hatch_pattern):
 
 def HatchPatternFillType(hatch_pattern):
     """Returns the fill type of a hatch pattern.
-        0 = solid, uses object color
-        1 = lines, uses pattern file definition
-        2 = gradient, uses fill color definition
     Parameters:
-      hatch_pattern = name of an existing hatch pattern
+      hatch_pattern (str): name of an existing hatch pattern
     Returns:
-      hatch pattern's fill type if successful otherwise None
+      number: hatch pattern's fill type if successful
+              0 = solid, uses object color
+              1 = lines, uses pattern file definition
+              2 = gradient, uses fill color definition
+      None: if unsuccessful
     Example:
       import rhinoscriptsyntax as rs
       patterns = rs.HatchPatternNames()
@@ -274,10 +273,8 @@ def HatchPatternFillType(hatch_pattern):
 
 def HatchPatternNames():
     """Returns the names of all of the hatch patterns in the document
-    Parameters:
-      None
     Returns:
-      the names of all of the hatch patterns in the document
+      list(str, ...): the names of all of the hatch patterns in the document
     Example:
       import rhinoscriptsyntax as rs
       patterns = rs.HatchPatternNames()
@@ -299,12 +296,12 @@ def HatchRotation(hatch_id, rotation=None):
     """Returns or modifies the rotation applied to the hatch pattern when
     it is mapped to the hatch's plane
     Parameters:
-      hatch_id = identifier of a hatch object
-      rotation[opt] = rotation angle in degrees
+      hatch_id (guid): identifier of a hatch object
+      rotation (number, optional): rotation angle in degrees
     Returns:
-      if rotation is not defined, the current rotation angle
-      if rotation is specified, the previous rotation angle
-      None on error
+      number: if rotation is not defined, the current rotation angle
+      number: if rotation is specified, the previous rotation angle
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       objects = rs.AllObjects()
@@ -336,12 +333,12 @@ def HatchScale(hatch_id, scale=None):
     """Returns or modifies the scale applied to the hatch pattern when it is
     mapped to the hatch's plane
     Parameters:
-      hatch_id = identifier of a hatch object
-      scale[opt] = scale factor
+      hatch_id (guid): identifier of a hatch object
+      scale (number, optional):  scale factor
     Returns:
-      if scale is not defined, the current scale factor
-      if scale is defined, the previous scale factor
-      None on error
+      number: if scale is not defined, the current scale factor
+      number: if scale is defined, the previous scale factor
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       objects = rs.NormalObjects()
@@ -368,9 +365,9 @@ def HatchScale(hatch_id, scale=None):
 def IsHatch(object_id):
     """Verifies the existence of a hatch object in the document
     Parameters:
-      object_id = identifier of an object
+      object_id (guid): identifier of an object
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       obj = rs.GetObject("Select object")
@@ -388,9 +385,9 @@ def IsHatch(object_id):
 def IsHatchPattern(name):
     """Verifies the existence of a hatch pattern in the document
     Parameters:
-      name = the name of a hatch pattern
+      name (str): the name of a hatch pattern
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       hatch = rs.GetString("Hatch pattern name")
@@ -406,10 +403,10 @@ def IsHatchPattern(name):
 def IsHatchPatternCurrent(hatch_pattern):
     """Verifies that a hatch pattern is the current hatch pattern
     Parameters:
-      hatch_pattern = name of an existing hatch pattern
+      hatch_pattern (str): name of an existing hatch pattern
     Returns:
-      True or False
-      None on error
+      bool: True or False
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       hatch = rs.GetString("Hatch pattern name")
@@ -431,10 +428,10 @@ def IsHatchPatternCurrent(hatch_pattern):
 def IsHatchPatternReference(hatch_pattern):
     """Verifies that a hatch pattern is from a reference file
     Parameters:
-      hatch_pattern = name of an existing hatch pattern
+      hatch_pattern (str): name of an existing hatch pattern
     Returns:
-      True or False
-      None on error
+      bool: True or False
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       hatch = rs.GetString("Hatch pattern name")
