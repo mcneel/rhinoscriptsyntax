6f3e6bf826830a4e52e5a4e29733458f6833c5e0
Scott Davidson <scottd@mcneel.com>
2017-03-06 15:19:17 -0800

    Updated the Docstring in light.py

--- Scripts/rhinoscript/light.py
+++ Scripts/rhinoscript/light.py
@@ -13,10 +13,10 @@ def __coercelight(id, raise_if_missing=False):
 def AddDirectionalLight(start_point, end_point):
     """Adds a new directional light object to the document
     Parameters:
-      start_point: starting point of the light
-      end_point: ending point and direction of the light
+      start_point(point): starting point of the light
+      end_point (point): ending point and direction of the light
     Returns:
-      identifier of the new object if successful
+      (guid): identifier of the new object if successful
     Example:
       import rhinoscriptsyntax as rs
       end = rs.GetPoint("End of light vector direction")
@@ -42,12 +42,12 @@ def AddDirectionalLight(start_point, end_point):
 def AddLinearLight(start_point, end_point, width=None):
     """Adds a new linear light object to the document
     Parameters:
-      start_point: starting point of the light
-      end_point: ending point and direction of the light
-      width[opt]: width of the light
+      start_point (point): starting point of the light
+      end_point (point): ending point and direction of the light
+      width (number): width of the light
     Returns:
-      identifier of the new object if successful
-      None on error
+      guid: identifier of the new object if successful
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       start = rs.GetPoint("Light origin")
@@ -89,9 +89,9 @@ def AddLinearLight(start_point, end_point, width=None):
 def AddPointLight(point):
     """Adds a new point light object to the document
     Parameters:
-      point = the 3d location of the point
+      point (point): the 3d location of the point
     Returns:
-      identifier of the new object if successful
+      guid: identifier of the new object if successful
     Example:
       import rhinoscriptsyntax as rs
       point = rs.GetPoint("Point light location")
@@ -113,11 +113,11 @@ def AddPointLight(point):
 def AddRectangularLight(origin, width_point, height_point):
     """Adds a new rectangular light object to the document
     Parameters:
-      origin = 3d origin point of the light
-      width_point = 3d width and direction point of the light
-      height_point = 3d height and direction point of the light
+      origin (point): 3d origin point of the light
+      width_point (point): 3d width and direction point of the light
+      height_point (point): 3d height and direction point of the light
     Returns:
-      identifier of the new object if successful
+      guid: identifier of the new object if successful
     Example:
       import rhinoscriptsyntax as rs
       rect = rs.GetRectangle(2)
@@ -148,11 +148,11 @@ def AddRectangularLight(origin, width_point, height_point):
 def AddSpotLight(origin, radius, apex_point):
     """Adds a new spot light object to the document
     Parameters:
-      origin = 3d origin point of the light
-      radius = radius of the cone
-      apex_point = 3d apex point of the light
+      origin (point): 3d origin point of the light
+      radius (number):  radius of the cone
+      apex_point (point): 3d apex point of the light
     Returns:
-      identifier of the new object
+      guid: identifier of the new object
     Example:
       import rhinoscriptsyntax as rs
       radius = 5.0
@@ -184,12 +184,12 @@ def AddSpotLight(origin, radius, apex_point):
 def EnableLight(object_id, enable=None):
     """Enables or disables a light object
     Parameters:
-      object_id = the light object's identifier
-      enable[opt] = the light's enabled status
+      object_id (guid): the light object's identifier
+      enable (bool, optional): the light's enabled status
     Returns:
-      if enable is not specified, the current enabled status 
-      if enable is specified, the previous enabled status
-      None on error
+      bool: if enable is not specified, the current enabled status
+      bool: if enable is specified, the previous enabled status
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select light", rs.filter.light)
@@ -215,9 +215,9 @@ def EnableLight(object_id, enable=None):
 def IsDirectionalLight(object_id):
     """Verifies a light object is a directional light
     Parameters:
-      object_id = the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -235,9 +235,9 @@ def IsDirectionalLight(object_id):
 def IsLight(object_id):
     """Verifies an object is a light object
     Parameters:
-      object_id: the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light")
@@ -260,9 +260,9 @@ def IsLight(object_id):
 def IsLightEnabled(object_id):
     """Verifies a light object is enabled
     Parameters:
-      object_id: the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -285,9 +285,9 @@ def IsLightEnabled(object_id):
 def IsLightReference(object_id):
     """Verifies a light object is referenced from another file
     Parameters:
-      object_id: the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -305,9 +305,9 @@ def IsLightReference(object_id):
 def IsLinearLight(object_id):
     """Verifies a light object is a linear light
     Parameters:
-      object_id = the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -325,9 +325,9 @@ def IsLinearLight(object_id):
 def IsPointLight(object_id):
     """Verifies a light object is a point light
     Parameters:
-      object_id = the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -345,9 +345,9 @@ def IsPointLight(object_id):
 def IsRectangularLight(object_id):
     """Verifies a light object is a rectangular light
     Parameters:
-      object_id = the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -365,9 +365,9 @@ def IsRectangularLight(object_id):
 def IsSpotLight(object_id):
     """Verifies a light object is a spot light
     Parameters:
-      object_id = the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      True or False
+      bool: True or False
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -387,11 +387,11 @@ def IsSpotLight(object_id):
 def LightColor(object_id, color=None):
     """Returns or changes the color of a light
     Parameters:
-      object_id = the light object's identifier
-      color[opt] = the light's new color
+      object_id (guid): the light object's identifier
+      color (color, optional): the light's new color
     Returns:
-      if color is not specified, the current color 
-      if color is specified, the previous color
+      color: if color is not specified, the current color
+      color: if color is specified, the previous color
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -419,10 +419,8 @@ def LightColor(object_id, color=None):
 
 def LightCount():
     """Returns the number of light objects in the document
-    Parameters:
-      None
     Returns:
-      the number of light objects in the document
+      number: the number of light objects in the document
     Example:
       import rhinoscriptsyntax as rs
       print "There are", rs.LightCount(), " lights."
@@ -440,15 +438,15 @@ def LightCount():
 def LightDirection(object_id, direction=None):
     """Returns or changes the direction of a light object
     Parameters:
-      object_id = the light object's identifier
-      direction[opt] = the light's new direction
+      object_id (guid): the light object's identifier
+      direction (vector, optional): the light's new direction
     Returns:
-      if direction is not specified, the current direction
-      if direction is specified, the previous direction
+      vector: if direction is not specified, the current direction
+      vector: if direction is specified, the previous direction
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
-      if id: rs.AddPoint( rs.LightDirection(id) )
+      if id: print( rs.LightDirection(id) )
     See Also:
       IsLight
       LightLocation
@@ -469,11 +467,11 @@ def LightDirection(object_id, direction=None):
 def LightLocation(object_id, location=None):
     """Returns or changes the location of a light object
     Parameters:
-      object_id = the light object's identifier
-      location[opt] = the light's new location
+      object_id (guid): the light object's identifier
+      location (point, optional): the light's new location
     Returns:
-      if location is not specified, the current location
-      if location is specified, the previous location
+      point: if location is not specified, the current location
+      point: if location is specified, the previous location
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -498,11 +496,11 @@ def LightLocation(object_id, location=None):
 def LightName(object_id, name=None):
     """Returns or changes the name of a light object
     Parameters:
-      object_id = the light object's identifier
-      name[opt] = the light's new name
+      object_id (guid): the light object's identifier
+      name (str, optional): the light's new name
     Returns:
-      if name is not specified, the current name
-      if name is specified, the previous name
+      str: if name is not specified, the current name
+      str: if name is specified, the previous name
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -530,10 +528,8 @@ def LightName(object_id, name=None):
 
 def LightObjects():
     """Returns list of identifiers of light objects in the document
-    Parameters:
-      None
     Returns:
-      the list of identifiers of light objects in the document
+      list(guid, ...): the list of identifiers of light objects in the document
     Example:
       import rhinoscriptsyntax as rs
       lights = rs.LightObjects()
@@ -559,10 +555,10 @@ def LightObjects():
 def RectangularLightPlane(object_id):
     """Returns the plane of a rectangular light object
     Parameters:
-      object_id = the light object's identifier
+      object_id (guid): the light object's identifier
     Returns:
-      the plane if successful
-      None on error
+      plane: the plane if successful
+      None: on error
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a rectangular light", rs.filter.light)
@@ -589,11 +585,11 @@ def SpotLightHardness(object_id, hardness=None):
     """Returns or changes the hardness of a spot light. Spotlight hardness
     controls the fully illuminated region.
     Parameters:
-      object_id = the light object's identifier
-      hardness[opt] = the light's new hardness
+      object_id (guid): the light object's identifier
+      hardness (number, optional): the light's new hardness
     Returns:
-      if hardness is not specified, the current hardness
-      if hardness is specified, the previous hardness
+      number: if hardness is not specified, the current hardness
+      number: if hardness is specified, the previous hardness
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -620,11 +616,11 @@ def SpotLightHardness(object_id, hardness=None):
 def SpotLightRadius(object_id, radius=None):
     """Returns or changes the radius of a spot light.
     Parameters:
-      object_id = the light object's identifier
-      radius[opt] = the light's new radius
+      object_id (guid): the light object's identifier
+      radius (number, optional): the light's new radius
     Returns:
-      if radius is not specified, the current radius
-      if radius is specified, the previous radius
+      number: if radius is not specified, the current radius
+      number: if radius is specified, the previous radius
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
@@ -653,11 +649,11 @@ def SpotLightRadius(object_id, radius=None):
 def SpotLightShadowIntensity(object_id, intensity=None):
     """Returns or changes the shadow intensity of a spot light.
     Parameters:
-      object_id = the light object's identifier
-      intensity[opt] = the light's new intensity
+      object_id (guid): the light object's identifier
+      intensity (number, optional): the light's new intensity
     Returns:
-      if intensity is not specified, the current intensity
-      if intensity is specified, the previous intensity
+      number: if intensity is not specified, the current intensity
+      number: if intensity is specified, the previous intensity
     Example:
       import rhinoscriptsyntax as rs
       id = rs.GetObject("Select a light", rs.filter.light)
