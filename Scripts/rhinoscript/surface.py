mcneel/rhinoscriptsyntax

commit cd443080fba2b3776d61d103b7c41df7fa7752d3

fix optional selection

Scripts/rhinoscript/surface.py
@@ -1055,7 +1055,9 @@ def DuplicateEdgeCurves(object_id, select=False):
             curve.Dispose()
             if rc==System.Guid.Empty: return None
             curves.append(rc)
-            if select: rhobject.SelectObject(rc)
+            if select: 
+                rhobject = rhutil.coercerhinoobject(rc)
+                rhobject.Select(true)
     if curves: scriptcontext.doc.Views.Redraw()
     return curves
 

commit 51db99a1c32238350b3f713d172808ccd5e8d301

fix typo introduced in cd443080fba2b3776d61d103b7c41df7fa7752d3

Scripts/rhinoscript/surface.py
@@ -1057,7 +1057,7 @@ def DuplicateEdgeCurves(object_id, select=False):
             curves.append(rc)
             if select: 
                 rhobject = rhutil.coercerhinoobject(rc)
-                rhobject.Select(true)
+                rhobject.Select(True)
     if curves: scriptcontext.doc.Views.Redraw()
     return curves
 