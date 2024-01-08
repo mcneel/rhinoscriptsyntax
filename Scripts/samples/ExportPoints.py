fe4aa8b17b3b51cfb32f3217a9d14ff3af8bb2cd
Scott Davidson <scottd@mcneel.com>
2024-01-08 14:20:52 -0800

    Small update on sample for 8

--- Scripts/samples/ExportPoints.py
+++ Scripts/samples/ExportPoints.py
@@ -11,18 +11,26 @@ def ExportPoints():
     filename = rs.SaveFileName("Save point coordinates as", filter)
     if( filename==None ): return
     
-    file = open(filename, "w")
-    for id in objectIds:
-        #process point clouds
-        if( rs.IsPointCloud(id) ):
-            points = rs.PointCloudPoints(id)
-            for pt in points:
-                file.writeline(str(pt))
-        elif( rs.IsPoint(id) ):
-            point = rs.PointCoordinates(id)
-            file.writeline(str(point))
-    file.close()
-
+    """
+    Using a 'with' loop to open the file, we do not need to clean
+    up or close the file when we are done, Python takes care of it.
+    Here, we'll write the points with a line break, otherwise
+    all the points will end up on one line.
+    """
+    with open(filename, "w")as file:
+        
+        for id in objectIds:
+            #process point clouds
+            if( rs.IsPointCloud(id) ):
+                points = rs.PointCloudPoints(id)
+                for pt in points:
+                    # convert the point list to a string, 
+                    # add a new line character, and write to the file
+                    file.write(str(pt)+ "\n")
+            elif( rs.IsPoint(id) ):
+                point = rs.PointCoordinates(id)
+                file.write(str(point)+ "\n")
+    
 
 ##########################################################################
 # Here we check to see if this file is being executed as the "main" python
